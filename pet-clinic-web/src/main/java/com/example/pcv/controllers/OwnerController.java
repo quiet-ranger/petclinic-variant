3244bd981c30f374860739427f5a3f2fd522a060
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2022-04-07 18:42:29 +0200

    Added index page and controller to handle owners path Closes #13

---
+++ pet-clinic-web/src/main/java/com/example/pcv/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.example.pcv.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html", "/index.htm"})
+    public String listVets() {
+        return "owners/index";
+    }
+
+}
