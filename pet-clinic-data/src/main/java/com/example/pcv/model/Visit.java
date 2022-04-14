36d094f45265fb8768acc0b702547c7a14c48747
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2022-04-14 09:40:24 +0200

    Created Visit entity and updated Owner with a set of Pets

---
+++ pet-clinic-data/src/main/java/com/example/pcv/model/Visit.java
@@ -0,0 +1,34 @@
+package com.example.pcv.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Long petId;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPetId() {
+        return petId;
+    }
+
+    public void setPet(Long petId) {
+        this.petId = petId;
+    }
+}
