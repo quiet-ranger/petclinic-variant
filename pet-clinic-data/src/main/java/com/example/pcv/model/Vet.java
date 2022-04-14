03cf805bf48893dd9e566266f8c40ff1a4f893b5
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2022-04-14 10:16:21 +0200

    Created Visit entity and updated Owner with a set of Pets

--- pet-clinic-data/src/main/java/com/example/pcv/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/pcv/model/Vet.java
@@ -1,6 +1,19 @@
 package com.example.pcv.model;
 
+import java.util.Set;
+
 public class Vet extends Person implements Comparable<Vet> {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
     @Override
     public int compareTo(Vet vet) {
         return this.getId().compareTo(vet.getId());
