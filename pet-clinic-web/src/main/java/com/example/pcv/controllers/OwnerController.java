37e89a396d6e0afc02e7fba1d6d85d5a5ce71e8e
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2022-04-25 19:51:17 +0200

    Using @InitBinder

--- pet-clinic-web/src/main/java/com/example/pcv/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/pcv/controllers/OwnerController.java
@@ -5,13 +5,15 @@ import com.example.pcv.model.OwnerSorterByFirstName;
 import com.example.pcv.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 @RequestMapping("/owners")
 @Controller
@@ -23,11 +25,16 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html", "/index.htm", "/find"})
     public String listOwners(Model model) {
 
         ArrayList<Owner> allOwners = new ArrayList<>(ownerService.findAll());
-        List.sort(allOwners, new OwnerSorterByFirstName());
+        Collections.sort(allOwners, new OwnerSorterByFirstName());
         model.addAttribute("owners", allOwners);
         return "owners/index";
 
