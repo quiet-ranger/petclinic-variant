fbdaf4412640452fde6ec7a879518dc44ab362ef
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2022-05-09 21:03:49 +0200

    Implemented a REST endpoint that returns JSON in the same controller that handles regular pages

--- pet-clinic-web/src/main/java/com/example/pcv/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/pcv/controllers/VetController.java
@@ -4,7 +4,9 @@ import com.example.pcv.model.Vet;
 import com.example.pcv.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,13 +21,17 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets.html", "/vets/index", "/vets/index.html", "/vets/index.htm"})
+    @GetMapping({"/vets.html", "/vets/index", "/vets/index.html", "/vets/index.htm"})
     public String listVets(Model model) {
 
         ArrayList<Vet> allVets = new ArrayList<>(vetService.findAll());
         Collections.sort(allVets);
         model.addAttribute("vets", allVets );
         return "vets/index";
+    }
 
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
